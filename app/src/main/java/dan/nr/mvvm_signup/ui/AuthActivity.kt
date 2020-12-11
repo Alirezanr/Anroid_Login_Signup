75c3a503040cc63965bab44cd8d870e624237aed
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-11 20:01:44 +0330

    create AuthActivity

---
+++ app/src/main/java/dan/nr/mvvm_signup/ui/AuthActivity.kt
@@ -0,0 +1,11 @@
+package dan.nr.mvvm_signup.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
