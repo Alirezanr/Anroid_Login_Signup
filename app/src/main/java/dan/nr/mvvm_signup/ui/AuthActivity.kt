4176dcbe8e69f405c0a8b6e229944994aaf2ac24
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-11 20:50:11 +0330

    create AuthApi interface for api calls

--- app/src/main/java/dan/nr/mvvm_signup/ui/AuthActivity.kt
+++ app/src/main/java/dan/nr/mvvm_signup/ui/AuthActivity.kt
@@ -2,10 +2,12 @@ package dan.nr.mvvm_signup.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dan.nr.mvvm_signup.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
     }
 }
--- app/src/main/java/dan/nr/mvvm_signup/ui/fragment/auth/LoginFragment.kt
+++ app/src/main/java/dan/nr/mvvm_signup/ui/fragment/auth/LoginFragment.kt
@@ -35,7 +35,7 @@ class LognFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logn, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     companion object {
