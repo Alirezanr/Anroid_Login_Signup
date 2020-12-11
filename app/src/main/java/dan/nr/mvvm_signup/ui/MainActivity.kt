d20530853a3a850f252ae50c830f433c916a4027
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-11 19:46:13 +0330

    init commit.finish ui design

---
+++ app/src/main/java/dan/nr/mvvm_signup/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package dan.nr.mvvm_signup.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dan.nr.mvvm_signup.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
