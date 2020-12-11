0818c3a792c81ef9b5aac0b3539c042b8202d11a
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-11 23:10:07 +0330

    create BaseRepositoty

--- app/src/main/java/dan/nr/mvvm_signup/network/Resource.kt
+++ app/src/main/java/dan/nr/mvvm_signup/network/Resource.kt
@@ -2,12 +2,11 @@ package dan.nr.mvvm_signup.network
 
 import okhttp3.ResponseBody
 
-sealed class Resource <out T>
-{
-    data class Success<out T>(val value:T):Resource<T>()
+sealed class Resource<out T> {
+    data class Success<out T>(val value: T) : Resource<T>()
     data class Failure(
-        val isNetworkError:Boolean,
-        val errorCode:Int?,
-        val errorBody:ResponseBody?
-    )
+        val isNetworkError: Boolean,
+        val errorCode: Int?,
+        val errorBody: ResponseBody?
+    ):Resource<Nothing>()
 }
---
+++ app/src/main/java/dan/nr/mvvm_signup/repository/BaseRepository.kt
@@ -0,0 +1,26 @@
+package dan.nr.mvvm_signup.repository
+
+import dan.nr.mvvm_signup.network.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+abstract class BaseRepository {
+
+    suspend fun <T> safeApiCall(apiCall: suspend () -> T): Resource<T> {
+        return withContext(Dispatchers.IO) {
+            try {
+                Resource.Success(apiCall.invoke())
+            } catch (t: Throwable) {
+                when (t) {
+                    is HttpException -> {
+                        Resource.Failure(false, t.code(), t.response()?.errorBody())
+                    }
+                    else -> {
+                        Resource.Failure(true, null, null)
+                    }
+                }
+            }
+        }
+    }
+}
