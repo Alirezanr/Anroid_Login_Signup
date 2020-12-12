8fac0194ca2eaa8cd4330f537710cf42f402b0a7
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-12 19:15:58 +0330

    finish BaseFragment.implement BaseFragment in LoginFragment

--- app/src/main/java/dan/nr/mvvm_signup/ui/fragment/auth/LoginFragment.kt
+++ app/src/main/java/dan/nr/mvvm_signup/ui/fragment/auth/LoginFragment.kt
@@ -6,55 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dan.nr.mvvm_signup.R
+import dan.nr.mvvm_signup.databinding.FragmentLoginBinding
+import dan.nr.mvvm_signup.network.AuthApi
+import dan.nr.mvvm_signup.repository.AuthRepository
+import dan.nr.mvvm_signup.ui.fragment.base.BaseFragment
+import dan.nr.mvvm_signup.ui.viewmodel.AuthViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LognFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LognFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+class LognFragment : BaseFragment<AuthViewModel, FragmentLoginBinding, AuthRepository>()
+{
+    override fun onActivityCreated(savedInstanceState: Bundle?)
+    {
+        super.onActivityCreated(savedInstanceState)
+        viewModel
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
-    }
+    override fun getViewModel() = AuthViewModel::class.java
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LognFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LognFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    override fun getFragmentBinding(inflater: LayoutInflater,
+                                    container: ViewGroup?
+                                   ) = FragmentLoginBinding.inflate(inflater, container, false)
+
+    override fun getFragmentRepository() = AuthRepository(remoteDataSource.buildApi(AuthApi::class.java))
+
+
 }
--- app/src/main/java/dan/nr/mvvm_signup/ui/fragment/base/BaseFragment.kt
+++ app/src/main/java/dan/nr/mvvm_signup/ui/fragment/base/BaseFragment.kt
@@ -6,19 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import dan.nr.mvvm_signup.network.RemoteDataSource
 import dan.nr.mvvm_signup.repository.BaseRepository
 
-abstract class BaseFragment<VM : ViewModel, B : ViewBinding, R : BaseRepository> : Fragment() {
+abstract class BaseFragment<VM : ViewModel, B : ViewBinding, R : BaseRepository> : Fragment()
+{
     protected lateinit var binding: B
-
+    protected lateinit var viewModel:VM
+    protected val remoteDataSource = RemoteDataSource()
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding=getFragmentBinding(inflater,container)
-
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+                             ): View?
+    {
+        binding = getFragmentBinding(inflater, container)
+        val factory=ViewModelFactory(getFragmentRepository())
+        viewModel=ViewModelProvider(this,factory).get(getViewModel())
         return binding.root
     }
 
