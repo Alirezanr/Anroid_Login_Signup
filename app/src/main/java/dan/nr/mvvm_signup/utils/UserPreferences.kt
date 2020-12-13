c6e2631b35ff87f1279af7f410d8c8cff03dedcc
Alireza Nori <alireza.nr1378@gmail.com>
2020-12-13 20:52:40 +0330

    create UserPreferences.kt to save auth token with the help of jetpack DataStor

---
+++ app/src/main/java/dan/nr/mvvm_signup/utils/UserPreferences.kt
@@ -0,0 +1,39 @@
+package dan.nr.mvvm_signup.utils
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.createDataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.preferencesKey
+import androidx.datastore.preferences.createDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class UserPreferences(context: Context)
+{
+    private val applicationContext = context.applicationContext
+    private val dataStore: DataStore<Preferences>
+
+    init
+    {
+        dataStore = applicationContext.createDataStore(name = "my_data_store")
+    }
+
+    val authToken: Flow<String?>
+        get() = dataStore.data.map { preferences ->
+            preferences[AUTH_KEY]
+        }
+
+    suspend fun saveAuthToken(authToken: String)
+    {
+        dataStore.edit { preferences ->
+            preferences[AUTH_KEY] = authToken
+        }
+    }
+
+    companion object
+    {
+        private val AUTH_KEY = preferencesKey<String>("auth_key")
+    }
+}
